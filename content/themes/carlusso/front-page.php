c87a8b5f8b4e1522bef02665103258ee7c21277e
LunarJim <jim.deghaye@gmail.com>
2019-04-29 17:26:40 +0200

    ajout de la boucle full voitures sur la front

--- content/themes/carlusso/front-page.php
+++ content/themes/carlusso/front-page.php
@@ -24,7 +24,7 @@
 
             ?>
 
-            <div class="carousel-item active">
+            <div class="carousel-item active" data-interval="2">
               <img src="<?= the_post_thumbnail_url(); ?>
               " class="" alt="...">
             </div>
@@ -45,7 +45,7 @@
             <?php $postImages = getPostImages($size = 'full'); ?>
             <?php foreach($postImages as $image): ?>
 
-            <div class="carousel-item">
+            <div class="carousel-item" >
               <img src="<?= $image ?>" class="" alt="...">
             </div>
 
@@ -76,104 +76,39 @@
       
   
       <div class="row cards justify-content-center">
+
+
+      <?php     $arg = [
+                'post_type' => 'Voiture',
+                'posts_per_page' => -1
+            ];
+            
+            $wp_query = new WP_Query($arg);
+
+            if ($wp_query->have_posts()): while($wp_query->have_posts()): $wp_query->the_post();
+
+            ?>
+
           <div class="card" style="width: 19rem;">
-            <img src="images/bm5.JPG" class="card-img-top" alt="...">
+            <img src="<?= the_post_thumbnail_url(); ?>" class="card-img-top" alt="...">
               <div class="card-body">
-                <h5 class="card-title">BMW Série 5</h5>
+                <h6 class="card-title"><?= the_title(); ?></h6>
                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">km : 450 000</li>
-                  <li class="list-group-item">AM : 1999</li>
-                  <li class="list-group-item">Prix : 5 Euros</li>
+                  <li class="list-group-item">km : <?= get_field('kilomètres'); ?></li>
+                  <li class="list-group-item">AM : <?= get_field('année'); ?></li>
+                  <li class="list-group-item">Prix : <?= get_field('prix'); ?></li>
                 </ul>
-                <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                
+                <a href="<?= get_post_permalink(); ?>" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
               </div>
           </div>
-          <div class="card" style="width: 19rem;">
-              <img src="images/picasso.JPG" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Citron Pikatchou</h5>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">km : 900 000</li>
-                    <li class="list-group-item">AM : 2020</li>
-                    <li class="list-group-item">Prix : 17 Euros</li>
-                  </ul>
-                  <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                </div>
-            </div>
-            <div class="card" style="width: 19rem;">
-                <img src="images/megane.JPG" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">meguan 7</h5>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">km : peu</li>
-                      <li class="list-group-item">AM : 2017</li>
-                      <li class="list-group-item">Prix : peu</li>
-                    </ul>
-                    <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                  </div>
-              </div>
-              <div class="card" style="width: 19rem;">
-                  <img src="images/bm1.JPG" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">BéhèmeV 1ère série</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Cras justo odio</li>
-                        <li class="list-group-item">Dapibus ac facilisis in</li>
-                        <li class="list-group-item">Vestibulum at eros</li>
-                      </ul>
-                      <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                    </div>
-                </div>
-                <div class="card" style="width: 19rem;">
-                    <img src="images/touran.JPG" class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">VW tour âne</h5>
-                        <ul class="list-group list-group-flush">
-                          <li class="list-group-item">Cras justo odio</li>
-                          <li class="list-group-item">Dapibus ac facilisis in</li>
-                          <li class="list-group-item">Vestibulum at eros</li>
-                        </ul>
-                        <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                      </div>
-                  </div>
-                  <div class="card" style="width: 19rem;">
-                      <img src="images/alfaRomeoGiulia.jpeg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Cras justo odio</li>
-                            <li class="list-group-item">Dapibus ac facilisis in</li>
-                            <li class="list-group-item">Vestibulum at eros</li>
-                          </ul>
-                          <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                        </div>
-                    </div>
-                    <div class="card" style="width: 19rem;">
-                        <img src="images/ferrari.jpeg" class="card-img-top" alt="...">
-                          <div class="card-body">
-                            <h5 class="card-title">250 GTO</h5>
-                            <ul class="list-group list-group-flush">
-                              <li class="list-group-item">...</li>
-                              <li class="list-group-item">...</li>
-                              <li class="list-group-item">...</li>
-                            </ul>
-                            <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                          </div>
-                      </div>
-                      <div class="card" style="width: 19rem;">
-                          <img src="images/titanic.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                              <h5 class="card-title">épave Titanic</h5>
-                              <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Km : à évaluer</li>
-                                <li class="list-group-item">AM : 1910 et des bananes</li>
-                                <li class="list-group-item">Prix : à évaluer aussi</li>
-                              </ul>
-                              <a href="#" class="btn btn-secondary mt-2 ml-5 fiche">Fiche détaillée</a>
-                            </div>
-                        </div>
-                        
+          
+          <?php
+
+          wp_reset_postdata();
+
+          endwhile;endif;
+          ?>
+
       </div>
 
       <?php get_footer(); ?>
